eb36c4b1f4a28cdf6afc75cc7e2dc346d806fcac
Pilllico <douglas1995@hotmail.fr>
2020-11-16 16:02:14 +0100

    Initial commit

--- GRAPHICS/engine/main.cpp
+++ GRAPHICS/engine/main.cpp
@@ -1,9 +1,11 @@
+#include "mainwindow.h"
 
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-
-
-
-    return;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
