4784cb0dbea1b145c796e9b87bc0a1d973c5d7f6
Luc Senecal <luc.senecal14@wanadoo.fr>
2021-01-03 15:40:47 +0100

    Premier ajout pour bullet

--- STRUCTURE/engine/ecs/physicssystem.cpp
+++ STRUCTURE/engine/ecs/physicssystem.cpp
@@ -3,4 +3,19 @@
 physicsSystem::physicsSystem()
 {
     systemSignature = bitmap("00001001");
+
+    collisionConfiguration = new btDefaultCollisionConfiguration ();
+    dispatcher = new btCollisionDispatcher ( collisionConfiguration );
+    overlappingPairCache = new btDbvtBroadphase ();
+    solver = new btSequentialImpulseConstraintSolver ;
+    dynamicsWorld = new btDiscreteDynamicsWorld ( dispatcher, overlappingPairCache, solver, collisionConfiguration);
+}
+
+void physicsSystem::execute()
+{
+    delete collisionConfiguration;
+    delete dispatcher;
+    delete overlappingPairCache;
+    delete solver;
+    delete dynamicsWorld;
 }
--- STRUCTURE/engine/ecs/physicssystem.h
+++ STRUCTURE/engine/ecs/physicssystem.h
@@ -2,12 +2,19 @@
 #define PHYSICSSYSTEM_H
 
 #include "system.h"
+#include "bullet/btBulletDynamicsCommon.h"
 
 class physicsSystem : public System
 {
 public:
     physicsSystem();
     void virtual execute();
+private:
+    btDefaultCollisionConfiguration * collisionConfiguration;
+    btCollisionDispatcher * dispatcher;
+    btBroadphaseInterface * overlappingPairCache;
+    btSequentialImpulseConstraintSolver * solver;
+    btDiscreteDynamicsWorld * dynamicsWorld;
 };
 
 #endif // PHYSICSSYSTEM_H
