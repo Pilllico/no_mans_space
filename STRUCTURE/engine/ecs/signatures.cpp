a1a2b5f10d52df0492e27e3c917a5e97687615a2
Luc Senecal <luc.senecal14@wanadoo.fr>
2021-01-08 17:15:24 +0100

    move

--- STRUCTURE/engine/ecs/signatures.cpp
+++
@@ -1,7 +0,0 @@
-#include "entity.h"
-#include "transform.h"
-#include "physics.h"
-
-// Component
-bitmap Transform::componentSignature = bitmap("00000001");
-bitmap Physics::componentSignature = bitmap("00001000");
